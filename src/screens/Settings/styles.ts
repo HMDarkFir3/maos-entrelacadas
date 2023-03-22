069712558cf2bb75014e40fe534d8cbf0189d5f5
Henrique Marques <henrique_marques.dev@outlook.com>
2023-03-22 12:05:49 -0300

    fix: create status bar height constant

--- src/screens/Settings/styles.ts
+++ src/screens/Settings/styles.ts
@@ -1,7 +1,15 @@
 import styled from "styled-components/native";
 
+import { STATUS_BAR_HEIGHT } from "@utils/globalVariables";
+
 export const Container = styled.View`
   flex: 1;
 
+  padding: 0 24px;
+
   background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+  margin-top: ${STATUS_BAR_HEIGHT + }px;
 `;
