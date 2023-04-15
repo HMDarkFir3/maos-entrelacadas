c432467c8a05d1205b159bd4c525278e7468d6c6
Henrique Marques <henrique_marques.dev@outlook.com>
2023-04-15 11:23:21 -0300

    feat: remove android folder from project

---
+++ app.config.js
@@ -0,0 +1,36 @@
+export default ({ config }) => {
+  switch (process.env.APP_ENV) {
+    case 'production': {
+      return {
+        ...config,
+        name: 'Mãos Entrelaçadas',
+        android: {
+          package: 'com.hmdarkfire.maosentrelacadas',
+        },
+      };
+    }
+    case 'development': {
+      return {
+        ...config,
+        name: 'Mãos Entrelaçadas (DEVELOPMENT)',
+        android: {
+          package: 'com.hmdarkfire.maosentrelacadas.development',
+        },
+      };
+    }
+    case 'preview': {
+      return {
+        ...config,
+        name: 'Mãos Entrelaçadas (PREVIEW)',
+        android: {
+          package: 'com.hmdarkfire.maosentrelacadas.preview',
+        },
+      };
+    }
+    default: {
+      return {
+        ...config,
+      };
+    }
+  }
+};
