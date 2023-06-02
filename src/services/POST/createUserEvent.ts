95d0c38ca0aa8d7be5af1db6cba2ad612ad00edc
Henrique Marques <henrique_marques.dev@outlook.com>
2023-06-02 11:00:26 -0300

    feat: add type in POST

--- src/services/POST/createUserEvent.ts
+++ src/services/POST/createUserEvent.ts
@@ -1,7 +1,9 @@
 import { api } from '@services/api';
 
+import { EventsDTO } from '@dtos/EventsDTO';
+
 export const createUserEvent = async (userId: string, eventId: string) => {
-  const { data } = await api.patch(`/events/signup/${userId}/${eventId}`);
+  const { data } = await api.patch<EventsDTO.Response>(`/events/signup/${userId}/${eventId}`);
 
   return data;
 };
